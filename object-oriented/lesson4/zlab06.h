c5fff552ea67145371e4964bb6b23fc15e82a086
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-07-01 12:13:02 +0200

    Create zlab06.h

---
+++ object-oriented/lesson4/zlab06.h
@@ -0,0 +1,36 @@
+#ifndef LAB_2_PD_ZLAB06_H
+#define LAB_2_PD_ZLAB06_H
+
+#include "zlab03.h" 
+#include <iostream>
+
+using namespace std;
+
+class Obliczenia {
+protected:
+    const double pi = 3.14;
+public:
+    virtual double promienKolaWgPola() = 0;
+
+    virtual double promienOkreguWgObwodu() = 0;
+
+    virtual ~Obliczenia();
+};
+
+class Kwadrat : public Prostokat, public Obliczenia{
+public:
+
+    Kwadrat(string nazwa = "?", double bok = 0)
+            : Prostokat(nazwa, bok, bok) {}
+
+    ~Kwadrat() {
+        cout << "Kwadrat: " << nazwa << " konczy dzialanie" << endl;
+    }
+
+    string doTekstu() override;
+    double promienKolaWgPola() override;
+    double promienOkreguWgObwodu() override;
+};
+
+
+#endif //LAB_2_PD_ZLAB06_H
