084e5d81920e71728f77c4c2256059b26053f679
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-07-01 15:04:03 +0200

    Create prototypes.h

---
+++ algorithms/text_algorithms/prototypes.h
@@ -0,0 +1,14 @@
+#ifndef LAB_5_PROTOTYPY_H
+#define LAB_5_PROTOTYPY_H
+#include <string>
+
+using namespace std;
+
+string wczytajTekst();
+string wczytajWzorzec();
+void algorytmNaiwny(string a, string b);
+void Knuth(string a, string b);
+void Boyer(string a, string b, char q, char z);
+void menu();
+
+#endif //LAB_5_PROTOTYPY_H
