0c774ef56f45a6b26bedf7815d049db67a304a7c
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-07-01 12:54:04 +0200

    Update main.cpp

--- object-oriented/lesson4/main.cpp
+++ object-oriented/lesson4/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include "zlab10.h"
-
+#include "zlab03.h"
 
 int main(){
+//  Prostokat p("Prostokat",2,3);
+//  Kwadrat k("Kwadrat",4);
+//  cout << p << endl;
+//  cout << k << endl;
+
+//  Prostokat p("Prostokat",2,3);
+//  cout << p << endl;
+//  cin >> p;
+//  cout << p << endl;
+
+//  Prostokat p("Prostokat",2,3);
+//  cout << p << endl;
+//  p++;
+//  cout << p << endl;
+
+//  Prostokat p0("Prostokat",2,3);
+//  cout << "!p0=" <<!p0 << endl;
+//  Prostokat p1("Prostokat",0,0);
+//  cout << "!p1=" <<!p1 << endl;;
+
 
 }
