ae58422b01de4fcd4e609d583eb0a005b7ab3e06
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-07-01 11:45:52 +0200

    Create l2.h

---
+++ object-oriented/lesson2/l2.h
@@ -0,0 +1,21 @@
+#ifndef LAB_2_PD_ZLAB02_H
+#define LAB_2_PD_ZLAB02_H
+
+
+class Zlab02 {
+public:
+    void zadanie_2_3();
+    void zadanie_2_4();
+    void zadanie_2_5();
+    void menu();
+    void zadanie_2_7();
+    void zadanie_2_8();
+    void zadanie_2_9();
+    void zadanie_2_10(short min, short max);
+    double pobierzLiczbe();
+
+
+};
+
+
+#endif //LAB_2_PD_ZLAB02_H
