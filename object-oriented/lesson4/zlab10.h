e66405b0855b67dce95309946152200023caa510
PawelPastwa <148966220+PawelPastwa@users.noreply.github.com>
2024-07-01 12:41:45 +0200

    Create zlab10.h

---
+++ object-oriented/lesson4/zlab10.h
@@ -0,0 +1,23 @@
+#ifndef LAB_2_PD_ZLAB10_H
+#define LAB_2_PD_ZLAB10_H
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <set>
+#include <map>
+#include "generatorlosowy.h"
+using namespace std;
+
+class ZLab10 {
+public:
+    void wektor();
+    void wektor_sortowanie();
+    void zbior();
+    void mapa();
+
+};
+
+
+#endif //LAB_2_PD_ZLAB10_H
